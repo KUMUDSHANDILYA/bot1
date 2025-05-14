de8a9fd78e7e77541411512b0ea0678ddaceb399
KUMUDSHANDILYA <55797140+KUMUDSHANDILYA@users.noreply.github.com>
2025-05-14 10:08:41 +0530

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Collections.Generic;
+using System.Globalization;
 
 namespace NL2SQLBotFrameworkBot
 {
@@ -21,24 +22,41 @@ namespace NL2SQLBotFrameworkBot
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddHttpClient();  // Required for HttpClient injection
+            services.AddHttpClient();
 
-            services.AddControllers().AddNewtonsoftJson();
+            services.AddControllers().AddNewtonsoftJson(options =>
+            {
+                options.SerializerSettings.MaxDepth = HttpHelper.BotMessageSerializerSettings.MaxDepth;
+            });
 
-            // 1. Read allowed callers from config
-            var allowedCallers = Configuration.GetSection("AllowedCallers").Get<List<string>>();
+            // Authentication configuration with allowed callers and valid token issuers
+            services.AddSingleton(sp =>
+            {
+                var config = sp.GetService<IConfiguration>();
 
-            // 2. Register AuthenticationConfiguration with allowed callers
-            services.AddSingleton<AuthenticationConfiguration>(sp =>
-            new AuthenticationConfiguration
+                var allowedCallers = config.GetSection("AllowedCallers").Get<List<string>>();
+                var claimsValidator = new AllowedCallersClaimsValidator(allowedCallers);
+
+                var validTokenIssuers = new List<string>();
+                var tenantId = config.GetValue<string>(MicrosoftAppCredentials.MicrosoftAppTenantIdKey);
+
+                if (!string.IsNullOrWhiteSpace(tenantId))
                 {
-                    ClaimsValidator = new AllowedCallersClaimsValidator(allowedCallers),
-                });
+                    validTokenIssuers.Add(string.Format(CultureInfo.InvariantCulture, AuthenticationConstants.ValidTokenIssuerUrlTemplateV1, tenantId));
+                    validTokenIssuers.Add(string.Format(CultureInfo.InvariantCulture, AuthenticationConstants.ValidTokenIssuerUrlTemplateV2, tenantId));
+                    validTokenIssuers.Add(string.Format(CultureInfo.InvariantCulture, AuthenticationConstants.ValidGovernmentTokenIssuerUrlTemplateV1, tenantId));
+                    validTokenIssuers.Add(string.Format(CultureInfo.InvariantCulture, AuthenticationConstants.ValidGovernmentTokenIssuerUrlTemplateV2, tenantId));
+                }
 
-            // 3. Add required bot services
+                return new AuthenticationConfiguration
+                {
+                    ClaimsValidator = claimsValidator,
+                    ValidTokenIssuers = validTokenIssuers
+                };
+            });
+
             services.AddSingleton<BotFrameworkAuthentication, ConfigurationBotFrameworkAuthentication>();
             services.AddSingleton<IBotFrameworkHttpAdapter, AdapterWithErrorHandler>();
-
             services.AddTransient<IBot, Bots.EchoBot>();
         }
 
